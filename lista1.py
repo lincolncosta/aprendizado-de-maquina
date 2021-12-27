cd11210eb1156d45ebe58ee4f350815ee333da1a
tekpixo <lincoln.mcosta@gmail.com>
2021-12-27 12:18:07 -0300

    resolucao do exercicio 1 da questao 1

--- lista1.py
+++ lista1.py
@@ -1,2 +1,16 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('dataset.csv')
+df['split'] = np.random.randn(df.shape[0], 1)
+
+msk = np.random.rand(len(df)) <= 0.8
+
+train = df[msk]
+test = df[~msk]
+
+print(train)
 # Questão 1
-# 1)
+# 1) O Método dos Mínimos Quadrados (MMQ), ou Mínimos Quadrados Ordinários (MQO) ou OLS (do inglês Ordinary Least Squares) é uma técnica de otimização matemática que procura encontrar o melhor ajuste para um conjunto de dados tentando minimizar a soma dos quadrados das diferenças entre o valor estimado e os dados observados (tais diferenças são chamadas resíduos). Ela consiste em um estimador que minimiza a soma dos quadrados dos resíduos da regressão, de forma a maximizar o grau de ajuste do modelo aos dados observados. Um requisito para o método dos mínimos quadrados é que o fator imprevisível (erro) seja distribuído aleatoriamente e essa distribuição seja normal. O Teorema Gauss-Markov garante (embora indiretamente) que o estimador de mínimos quadrados é o estimador não-enviesado de mínima variância linear na variável resposta. O livro de Murphy (2012) explica a derivação em detalhes, expressa pela fórmula: wOLS = (XT * X) − 1XT * y.
+
+# 2) 
